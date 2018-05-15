4c26931e3c6650e5d6eb04e908d638f0c2ea4ae4
Jared Kobos <jaredkobos@gmail.com>
2018-05-15 17:14:05 -0400

    Add skeleton for Text record methods

--- main.go
+++ main.go
@@ -28,17 +28,36 @@ func DeleteUser(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "not implemented yet !")
 }
 
-// DeleteMovieEndPoint : Deletes a movie from the database.
-func DeleteMovieEndPoint(w http.ResponseWriter, r *http.Request) {
+// GetAllTexts : Returns a list of all reading texts in the database.
+func GetAllTexts(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "not implemented yet !")
+}
+
+// GetText : Retrieve a Text object by id.
+func GetText(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "not implemented yet !")
+}
+
+// UpdateText : Update a Text object in the database.
+func UpdateText(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "not implemented yet !")
+}
+
+// DeleteText : Deletes a Text record from the database.
+func DeleteText(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "not implemented yet !")
 }
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/users", GetAllUsers).Methods("GET")
-	r.HandleFunc("/users/{id}", GetUser).Methods("POST")
-	r.HandleFunc("/users/{id}", UpdateUser).Methods("PUT")
-	r.HandleFunc("/users/{id}", DeleteUser).Methods("DELETE")
+	r.HandleFunc("/user", GetAllUsers).Methods("GET")
+	r.HandleFunc("/user/{id}", GetUser).Methods("POST")
+	r.HandleFunc("/user/{id}", UpdateUser).Methods("PUT")
+	r.HandleFunc("/user/{id}", DeleteUser).Methods("DELETE")
+	r.HandleFunc("/text", GetAllTexts).Methods("GET")
+	r.HandleFunc("/text/{id}", GetText).Methods("POST")
+	r.HandleFunc("/text/{id}", UpdateText).Methods("PUT")
+	r.HandleFunc("/text/{id}", DeleteText).Methods("DELETE")
 	if err := http.ListenAndServe(":3000", r); err != nil {
 		log.Fatal(err)
 	}
